8c45b1f1781e28cb6ec000253a8b2f4c82db1aa7
juststartmeteor <juststartmeteor@gmail.com>
2015-03-12 21:34:03 -0400

    Add simple validation on input fields.

--- client/js/usersList.js
+++ client/js/usersList.js
@@ -9,17 +9,32 @@ Template.usersList.events({
 		}
 
 		if (confirm === true) {
-			var firstName = $("[name='first-name']").val();
-			var lastName = $("[name='last-name']").val();
-			var phoneNumber = $("[name='phone-number']").val();
-			var email = $("[name='email']").val();
-			var address = $("[name='address']").val();
-			Meteor.call("addUser", firstName, lastName, phoneNumber, email, address);
-			$("[name='first-name']").val('');
-			$("[name='last-name']").val('');
-			$("[name='phone-number']").val('');
-			$("[name='email']").val('');
-			$("[name='address']").val('');
+			if ($("[name='first-name']").val() === '') {
+				showRequiredField ("first-name");
+			} else if ($("[name='last-name']").val() === '') {
+				showRequiredField ("last-name");
+			} else if ($("[name='email']").val() === '') {
+				showRequiredField ("email");
+			} else {
+				var firstName = inputNameValid($("[name='first-name']").val());
+				var lastName = inputNameValid($("[name='last-name']").val());
+				var phoneNumber = inputPhoneNumberValid($("[name='phone-number']").val());
+				var email = inputEmailValid($("[name='email']").val());
+				var address = $("[name='address']").val();
+
+				if (firstName === false) {
+					showInputWrongField("first-name");
+				} else if (lastName === false) {
+					showInputWrongField("last-name");
+				} else if (email === false) {
+					showInputWrongField("email");
+				} else if (phoneNumber === false) {
+					showInputWrongField("phone-number");
+				} else {
+					Meteor.call("addUser", firstName, lastName, phoneNumber, email, address);
+					$(".add-new-user [type='text']").val('');
+				}
+			}
 		}
 	}
 });
@@ -36,4 +51,39 @@ Template.usersList.helpers({
 		}
 	}
 });
+
+function showRequiredField (attributeName) {
+	$(".add-new-user [type='text']").removeClass("required");
+	$("[name='" + attributeName + "']").addClass("required");
+}
+
+function showInputWrongField (attributeName) {
+	$(".add-new-user [type='text']").removeClass("notValid");
+	$("[name='" + attributeName + "']").addClass("notValid");
+}
+
+function inputNameValid(name) {
+	if (/^[a-zA-Z]+[\s]?[a-zA-Z]+$/.test(name)) {
+		return name;
+	} else {
+		return false;
+	}
+}
+
+function inputEmailValid(email) {
+	if (/^.+@.+\..+$/.test(email)) {
+		return email
+	} else {
+		return false;
+	}
+}
+
+function inputPhoneNumberValid(phoneNumber) {
+	if (/^[0-9]+$/.test(phoneNumber)) {
+		return phoneNumber
+	} else {
+		return false;
+	}
+}
+
 
--- server/methods.js
+++ server/methods.js
@@ -1,14 +1,14 @@
 Meteor.methods({
-			addUser: function (firstName, lastName, phoneNumber, email, address) {
-				Users.insert({firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, email: email, address: address, createdAt: new Date()});
-			},
-			updateUser: function (id, firstName, lastName, phoneNumber, email, address) {
-				Users.update(id, {$set: {firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, email: email, address: address, updatedAt: new Date()}});
-			},
-			updateEditStatus: function (id, editing) {
-				Users.update(id, {$set: {editing: editing}});
-			},
-			deleteAUser: function (id) {
-				Users.remove(id);
-			}
+	addUser: function (firstName, lastName, phoneNumber, email, address) {
+		Users.insert({firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, email: email, address: address, createdAt: new Date()});
+	},
+	updateUser: function (id, firstName, lastName, phoneNumber, email, address) {
+		Users.update(id, {$set: {firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, email: email, address: address, updatedAt: new Date()}});
+	},
+	updateEditStatus: function (id, editing) {
+		Users.update(id, {$set: {editing: editing}});
+	},
+	deleteAUser: function (id) {
+		Users.remove(id);
+	}
 });
